7800786bdd8e5211578ef23842bfcc68268f0a67
Siwat Techavoranant <keen@keendev.net>
2023-02-06 18:44:29 +0700

    feat: add division email in dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,7 +14,8 @@
             </div>
         </div>
         <p class="pb-6 max-w-7xl mx-auto sm:px-6 lg:px-8 text-xs text-gray-400 text-center">
-            สร้างสรรค์โดย ฝ่ายเทคโนโลยีสารสนเทศ สพจ. | <a href="https://github.com/docchula/smcu-admin" target="_blank" class="text-green-400">Source code</a>
+            สร้างสรรค์โดย ฝ่ายเทคโนโลยีสารสนเทศ สพจ. | <a href="https://github.com/docchula/smcu-admin" target="_blank" class="text-green-400">Source code</a><br/>
+            หากพบปัญหาในการใช้งาน ติดต่ออีเมล itdivision@docchula.com
         </p>
     </app-layout>
 </template>
