435f5932e9f0cd6c94591ae4e06e135d04e2242b
Siwat Techavoranant <5007387+keenthekeen@users.noreply.github.com>
2023-02-06 22:41:47 +0700

    fix: show dashboard alert only of my projects

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Project;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class Dashboard extends Controller {
     public function __invoke(Request $request) {
         return Inertia::render('Dashboard', [
-            'projectsAwaitingSummary' => Project::select(['id', 'number', 'year', 'name'])->whereDate('created_at', '>', now()->subYear())->whereNotIn('department_id', [32, 38, 39])->has('approvalDocument')->doesntHave('summaryDocument')->get(),
+            'projectsAwaitingSummary' => $request->user()->projects()->select(['id', 'number', 'year', 'name'])->whereDate('created_at', '>', now()->subYear())->whereNotIn('department_id', [32, 38, 39])->has('approvalDocument')->doesntHave('summaryDocument')->get(),
         ]);
     }
 }
