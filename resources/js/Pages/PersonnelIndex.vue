9843aaddd95f11d61593e4245e111a01b7a64679
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-01-13 09:01:47 +0700

    fix: moving add buttons

--- resources/js/Pages/PersonnelIndex.vue
+++ resources/js/Pages/PersonnelIndex.vue
@@ -62,7 +62,7 @@
         </div>
         <inertia-link v-if="is_admin" :href="route('personnels.create', {year})">
             <button
-                class="p-0 w-16 h-16 bg-pink-600 rounded-full hover:bg-pink-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none absolute bottom-6 right-6">
+                class="p-0 w-16 h-16 bg-pink-600 rounded-full hover:bg-pink-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none fixed bottom-6 right-6">
                 <svg class="w-6 h-6 inline-block" enable-background="new 0 0 20 20" viewBox="0 0 20 20">
                     <path d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
                                     C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
