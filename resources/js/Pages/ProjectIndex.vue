57f6e75adae7f63ebedadd8918f8c0b2edfb8efe
Ittipat <39715559+ittipatken@users.noreply.github.com>
2024-01-16 16:49:04 +0700

    fix: moving add buttons (#3)

--- resources/js/Pages/ProjectIndex.vue
+++ resources/js/Pages/ProjectIndex.vue
@@ -79,7 +79,7 @@
         </div>
         <inertia-link :href="route('projects.create')">
             <button
-                class="p-0 w-16 h-16 bg-yellow-600 rounded-full hover:bg-yellow-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none absolute bottom-6 right-6">
+                class="p-0 w-16 h-16 bg-yellow-600 rounded-full hover:bg-yellow-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none fixed bottom-6 right-6">
                 <svg viewBox="0 0 20 20" enable-background="new 0 0 20 20" class="w-6 h-6 inline-block">
                     <path fill="#FFFFFF" d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
                                     C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
