2dff8271ee0815ef351169768d2ce724d1c16cef
Siwat T. <keen@keendev.net>
2024-04-30 03:48:06 +0700

    feat: full expense table in ProjectShow

--- resources/js/Pages/ProjectShow.vue
+++ resources/js/Pages/ProjectShow.vue
@@ -163,14 +163,11 @@
             <div v-if="item.expense.length" class="bg-white shadow overflow-hidden sm:rounded-lg my-4">
                 <div class="px-4 py-5 sm:px-6">
                     <h3 class="text-lg leading-6 font-medium text-gray-900">
-                        ทรัพยากร
+                        งบประมาณ
                     </h3>
                 </div>
                 <div class="border-t border-gray-200">
-                    <dl>
-                        <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">งบประมาณ</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    <div class="px-4 py-4 sm:px-6">
                                 <table v-if="item.expense.length > 0" class="divide-y divide-gray-200">
                                     <thead>
                                     <tr>
@@ -213,9 +210,7 @@
                                     </tbody>
                                 </table>
                                 <span v-else>-</span>
-                            </dd>
                         </div>
-                    </dl>
                 </div>
             </div>
             <div v-if="item.documents.length > 0" class="bg-white shadow overflow-hidden sm:rounded-lg my-4">
@@ -226,7 +221,7 @@
                 </div>
                 <div class="border-t border-gray-200">
                     <div class="px-4 py-4 sm:px-6">
-                        <table class="divide-y divide-gray-200">
+                        <table class="w-full divide-y divide-gray-200">
                             <thead>
                             <tr>
                                 <th scope="col" class="px-2 pb-1 text-left text-xs font-medium text-gray-500 tracking-wider">
