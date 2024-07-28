dd1ac45828c3217b66ab427429709a99a66cd181
Siwat T. <keen@keendev.net>
2024-07-28 23:06:25 +0700

    feat: add health check in welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -15,13 +15,15 @@
                         </div-->
                         <div v-if="canLogin" class="mt-5 sm:mt-12 sm:flex sm:justify-center lg:justify-start">
                             <div v-if="$page.props.auth.user" class="mt-3 sm:mt-0 sm:ml-3">
-                                <inertia-link href="/dashboard"
+                                <Link :href="route('dashboard')"
                                               class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-green-700 bg-green-100 hover:bg-green-200 md:py-4 md:text-lg md:px-10">
                                     หน้าแรก
-                                </inertia-link>
+                                </Link>
                             </div>
                             <div v-else>
-                                <p class="block mb-4">เมื่อกดปุ่มต่อไปนี้ ถือว่าท่านได้ยินยอมให้มีการดำเนินการต่อข้อมูลส่วนบุคคลตาม<inertia-link :href="route('policy.show')" class="text-green-500">นโยบายการคุ้มครองข้อมูลส่วนบุคคล</inertia-link></p>
+                                <p class="block mb-4">เมื่อกดปุ่มต่อไปนี้ ถือว่าท่านได้ยินยอมให้มีการดำเนินการต่อข้อมูลส่วนบุคคลตาม
+                                    <Link :href="route('policy.show')" class="text-green-500">นโยบายการคุ้มครองข้อมูลส่วนบุคคล</Link>
+                                </p>
                                 <div class="rounded-md shadow">
                                     <a :href="route('login.google')"
                                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
@@ -31,6 +33,15 @@
                                 <p class="pt-4 text-xs text-gray-400">ท่านอาจไม่สามารถเข้าสู่ระบบได้จาก in-app browser (เช่น Facebook หรือ LINE) ให้นำ URL ไปเปิดในเว็บเบราเซอร์ เช่น Chrome หรือ Safari</p>
                             </div>
                         </div>
+                        <p v-if="healthCheck !== 0" class="mt-8 lg:mt-12 sm:mb-4 text-xs text-gray-400">
+                            สถานะ :
+                            <span v-if="healthCheck === 1" class="text-green-400">
+                                ⬤ ระบบพร้อมใช้งาน
+                            </span>
+                            <span v-else class="text-red-400">
+                                ⬤ ระบบมีปัญหา อาจไม่สามารถดู/บันทึกข้อมูลบางอย่างได้ กรุณาติดต่อผู้ดูแลระบบ
+                            </span>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -45,123 +56,23 @@
             </div>
         </div>
     </div>
-    <!--div class="items-top justify-center pb-12 bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div v-if="canLogin" class="px-6 py-4 sm:block text-right">
-            <inertia-link v-if="$page.props.auth.user" href="/dashboard" class="text-sm text-gray-700 underline">
-                Dashboard
-            </inertia-link>
-
-            <template v-else>
-                <a :href="route('login.google')" class="text-sm text-gray-700 underline">
-                    Log in using Docchula
-                </a>
-            </template>
-        </div>
-
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                <div class="grid grid-cols-1 md:grid-cols-2">
-                    <div class="p-6">
-                        <div class="flex items-center">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path
-                                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
-                            </svg>
-                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a></div>
-                        </div>
-
-                        <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we
-                                recommend reading all of the documentation from beginning to end.
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-                        <div class="flex items-center">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path
-                                    d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path>
-                                <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                            </svg>
-                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Laracasts</a></div>
-                        </div>
-
-                        <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development
-                                skills in the process.
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="p-6 border-t border-gray-200 dark:border-gray-700">
-                        <div class="flex items-center">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
-                            </svg>
-                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline text-gray-900 dark:text-white">Laravel News</a></div>
-                        </div>
-
-                        <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laravel News is a community driven portal and newsletter aggregating all of the latest and most important news in the Laravel ecosystem, including new package releases
-                                and tutorials.
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-                        <div class="flex items-center">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path
-                                    d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                            </svg>
-                            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</div>
-                        </div>
-
-                        <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" class="underline">Forge</a>, <a
-                                href="https://vapor.laravel.com" class="underline">Vapor</a>, <a href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io"
-                                                                                                                                                                    class="underline">Envoyer</a> help
-                                you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>,
-                                <a href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a
-                                href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a
-                                href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                <div class="text-center text-sm text-gray-500 sm:text-left">
-                    <div class="flex items-center">
-                        <a href="https://docchula.com" class="ml-1 hover:underline">
-                            สโมสรนิสิตคณะแพทยศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย
-                        </a>
-                    </div>
-                </div>
-
-                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                    ฝ่ายเทคโนโลยีสารสนเทศ
-                </div>
-            </div>
-        </div>
-    </div-->
 </template>
 
-<script>
+<script setup>
 import JetBanner from '@/Jetstream/Banner.vue';
+import {Link} from '@inertiajs/vue3';
+import {onMounted, ref} from 'vue';
+import axios from 'axios';
 
-export default {
-    components: {
-        JetBanner,
-    },
-    props: {
-        canLogin: Boolean,
-    }
-}
+defineProps({
+    canLogin: Boolean,
+});
+const healthCheck = ref(0);
+onMounted(() => {
+    axios.get('/health').then(response => {
+        healthCheck.value = response.data ? 1 : -1;
+    }).catch(() => {
+        healthCheck.value = -1;
+    });
+});
 </script>
