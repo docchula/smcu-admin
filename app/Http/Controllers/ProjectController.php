a64285501325eabce479acc0d63a54cd2ec3ccf2
Siwat T. <keen@keendev.net>
2024-10-30 12:14:35 +0700

    fix: skip import participant if lower rank

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -589,8 +589,7 @@ class ProjectController extends Controller {
             if ($existingParticipant = $project->participants->where('user_id', $student->id)->first()) {
                 if (ProjectParticipant::TYPES_RANK[$existingParticipant->type] < ProjectParticipant::TYPES_RANK[$row['type']]) {
                     $messages [] = 'WARNING: '.$row['student_id'].' มีอยู่แล้วและมีตำแหน่งที่สูงกว่า ไม่บันทึกข้อมูลใหม่';
-                }
-                if ($existingParticipant->type != $row['type'] or $existingParticipant->title != $row['title']) {
+                } elseif ($existingParticipant->type != $row['type'] or $existingParticipant->title != $row['title']) {
                     $toAdd->add([
                         'user_id' => $student->id,
                         'user_name' => $student->name,
